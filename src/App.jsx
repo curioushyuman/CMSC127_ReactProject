02e3d96bdfab84d43506a218559cb45f328e3c9c
Corally Into <csinto@up.edu.ph>
2023-05-08 20:54:37 +0800

    changes
    with header eme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import './App.css';
-import LoginPage from './components/LoginPage/LoginPage';
-import LandingPage from './components/LandingPage/LandingPage';
-import Header from "./components/Header/Header";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './components/header/header.module.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <LoginPage />
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      {/* HEADER  START*/}
+      <div class="navbar">
+      <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+      <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
+      <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
+      <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+      </div>
+      {/* HEADER  START*/}
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>,
+)
